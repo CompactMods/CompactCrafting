d2e7c009eee5293bd15ee626bdc22a7e94345c4d
Ted Senft <code@robotgryphon.com>
2020-09-24 04:01:17 -0400

    Example recipes, hide WIP field projection

--- src/main/java/com/robotgryphon/compactcrafting/client/render/FieldProjectorRenderer.java
+++ src/main/java/com/robotgryphon/compactcrafting/client/render/FieldProjectorRenderer.java
@@ -52,7 +52,7 @@ public class FieldProjectorRenderer extends TileEntityRenderer<FieldProjectorTil
         renderDish(tileEntityIn, matrixStackIn, bufferIn, combinedLightIn, combinedOverlayIn);
 
         AxisAlignedBB cube = new AxisAlignedBB(tileEntityIn.getPos()).grow(3);
-        renderFaces(matrixStackIn, bufferIn, cube, 0);
+        renderFaces(tileEntityIn, matrixStackIn, bufferIn, cube, 0);
     }
 
     private IBakedModel getModel() {
@@ -115,7 +115,142 @@ public class FieldProjectorRenderer extends TileEntityRenderer<FieldProjectorTil
         mx.pop();
     }
 
-    private void renderFaces(MatrixStack mx, IRenderTypeBuffer buffer, AxisAlignedBB cube, double extraLength) {
+    /**
+     * TODO - Main projector (single, NORTH projector)
+     * TODO - render scan line animated over main crafting projection
+     * TODO - render main crafting projection cube
+     *
+     * TODO - All projectors (individually)
+     * TODO - render projection arc connecting projector to large cube
+     */
+
+    /**
+     * Handles rendering the main projection cube in the center of the projection area.
+     * Should only be called by the main projector (typically the NORTH projector)
+     */
+    private void renderProjectionCube() {
+        // Draw the faces
+//        buffer.pos(x1, y1, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x1, y2, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y2, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y1, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x1, y1, z2).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y1, z2).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y2, z2).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x1, y2, z2).color(cR, cG, cB, cA).endVertex();
+//
+//        buffer.pos(x1, y1, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y1, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y1, z2).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x1, y1, z2).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x1, y2, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x1, y2, z2).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y2, z2).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y2, z1).color(cR, cG, cB, cA).endVertex();
+//
+//        buffer.pos(x1, y1, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x1, y1, z2).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x1, y2, z2).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x1, y2, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y1, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y2, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y2, z2).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y1, z2).color(cR, cG, cB, cA).endVertex();
+    }
+
+    /**
+     * Handles drawing the projection arcs that connect the projector blocks to the main projection
+     * in the center of the crafting area.
+     */
+    private void drawProjectorArcs() {
+//        // Render projection planes
+//        double zAngle = ((Math.sin(Math.toDegrees(RenderTickCounter.renderTicks) / -5000) + 1.0d) / 2) * (cube.maxY - cube.minY);
+//        double y3 = y1 + zAngle;
+//        float cA2 = 0.105f;
+//
+//        // Ensure both sides of the plane are visible
+//        GlStateManager.disableCull();
+//        GL11.glDisable(GL11.GL_CULL_FACE);
+//        // north -> south
+//        buffer.pos(x1, y3, z1).color(cR, cG, cB, cA2).endVertex();
+//        buffer.pos(x2, y3, z1).color(cR, cG, cB, cA2).endVertex();
+//        buffer.pos(x2-(radius-0.2f), y4, z1-(radius+0.8f+extraLength)).color(cR, cG, cB, cA2).endVertex();
+//        buffer.pos(x1+(radius-0.2f), y4, z1-(radius+0.8f+extraLength)).color(cR, cG, cB, cA2).endVertex();
+//
+//        // east -> west
+//        buffer.pos(x2, y3, z1).color(cR, cG, cB, cA2).endVertex();
+//        buffer.pos(x2, y3, z2).color(cR, cG, cB, cA2).endVertex();
+//        buffer.pos(x2+(radius+0.8f+extraLength), y4, z2-(radius-0.2f)).color(cR, cG, cB, cA2).endVertex();
+//        buffer.pos(x2+(radius+0.8f+extraLength), y4, z1+(radius-0.2f)).color(cR, cG, cB, cA2).endVertex();
+//
+//        // south -> north
+//        buffer.pos(x1, y3, z2).color(cR, cG, cB, cA2).endVertex();
+//        buffer.pos(x2, y3, z2).color(cR, cG, cB, cA2).endVertex();
+//        buffer.pos(x2-(radius-0.2f), y4, z2+(radius+0.8f+extraLength)).color(cR, cG, cB, cA2).endVertex();
+//        buffer.pos(x1+(radius-0.2f), y4, z2+(radius+0.8f+extraLength)).color(cR, cG, cB, cA2).endVertex();
+//
+//        // west -> east
+//        buffer.pos(x1, y3, z1).color(cR, cG, cB, cA2).endVertex();
+//        buffer.pos(x1, y3, z2).color(cR, cG, cB, cA2).endVertex();
+//        buffer.pos(x1-(radius+0.8f+extraLength), y4, z2-(radius-0.2f)).color(cR, cG, cB, cA2).endVertex();
+//        buffer.pos(x1-(radius+0.8f+extraLength), y4, z1+(radius-0.2f)).color(cR, cG, cB, cA2).endVertex();
+//
+//
+//        tessellator.draw();
+//        GlStateManager.enableCull();
+    }
+
+    /**
+     * Handles drawing the brighter "scan line" around the main projection cube. These lines show visibly
+     * where the projection arcs meet the main projection cube.
+     */
+    private void drawScanLines() {
+//        Tessellator tessellator = Tessellator.getInstance();
+//        BufferBuilder buffer = tessellator.getBuffer();
+//        buffer.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION_COLOR);
+//
+//        int color = 0xFF6A00;
+//        float cR = (color >> 16 & 255) / 255.0f;
+//        float cG = (color >> 8 & 255) / 255.0f;
+//        float cB = (color & 255) / 255.0f;
+//        float cA = 0.5f;
+//
+//        double x1 = cube.minX;
+//        double x2 = cube.maxX;
+//        double y1 = cube.minY;
+//        double y2 = cube.maxY;
+//        double z1 = cube.minZ;
+//        double z2 = cube.maxZ;
+//
+//        // Draw the up and down bouncing lines on the sides
+//        double zAngle = ((Math.sin(Math.toDegrees(RenderTickCounter.renderTicks) / -5000) + 1.0d) / 2) * (y2 - y1);
+//        double y3 = y1 + zAngle;
+//        buffer.pos(x1, y3, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y3, z1).color(cR, cG, cB, cA).endVertex();
+//
+//        buffer.pos(x1, y3, z1).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x1, y3, z2).color(cR, cG, cB, cA).endVertex();
+//
+//        buffer.pos(x2, y3, z2).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y3, z1).color(cR, cG, cB, cA).endVertex();
+//
+//        buffer.pos(x1, y3, z2).color(cR, cG, cB, cA).endVertex();
+//        buffer.pos(x2, y3, z2).color(cR, cG, cB, cA).endVertex();
+//
+//        tessellator.draw();
+    }
+
+    /**
+     *
+     * @param mx
+     * @param buffer
+     * @param cube
+     * @param extraLength
+     */
+    private void renderFaces(FieldProjectorTile tile, MatrixStack mx, IRenderTypeBuffer buffer, AxisAlignedBB cube, double extraLength) {
+
+        BlockState state = tile.getBlockState();
+        Direction facing = state.get(FieldProjectorBlock.FACING);
 
         try {
             IVertexBuilder builder = buffer.getBuffer(RenderTypesExtensions.PROJECTION_FIELD_RENDERTYPE);
@@ -126,16 +261,29 @@ public class FieldProjectorRenderer extends TileEntityRenderer<FieldProjectorTil
             // Draw the faces
             Vector3d start = new Vector3d(cube.minX, cube.minY, cube.minZ);
             Vector3d end = new Vector3d(cube.maxX, cube.maxY, cube.maxZ);
-            
-            addColoredVertex(builder, mx, fieldColor, new Vector3f(0, 0, .5f));
-            addColoredVertex(builder, mx, fieldColor, new Vector3f(1, 0, .5f));
-            addColoredVertex(builder, mx, fieldColor, new Vector3f(1, 1, .5f));
-            addColoredVertex(builder, mx, fieldColor, new Vector3f(0, 1, .5f));
 
-            addColoredVertex(builder, mx, fieldColor, new Vector3f(0, 1, .5f));
-            addColoredVertex(builder, mx, fieldColor, new Vector3f(1, 1, .5f));
-            addColoredVertex(builder, mx, fieldColor, new Vector3f(1, 0, .5f));
-            addColoredVertex(builder, mx, fieldColor, new Vector3f(0, 0, .5f));
+            Vector3f bl = new Vector3f(0, 0, 0);
+            Vector3f tr = new Vector3f(1, 1, 0);
+
+//            mx.push();
+
+//            float angle = facing.getHorizontalAngle() - 90;
+//            mx.rotate(Vector3f.YN.rotationDegrees(angle));
+//
+//            addColoredVertex(builder, mx, fieldColor, new Vector3f(bl.getX(), bl.getY(), bl.getZ()));
+//            addColoredVertex(builder, mx, fieldColor, new Vector3f(tr.getX(), bl.getY(), tr.getZ()));
+//            addColoredVertex(builder, mx, fieldColor, new Vector3f(tr.getX(), tr.getY(), tr.getZ()));
+//            addColoredVertex(builder, mx, fieldColor, new Vector3f(bl.getX(), tr.getY(), bl.getZ()));
+//
+//
+//
+//            mx.pop();
+
+
+//            addColoredVertex(builder, mx, fieldColor, new Vector3f(0, 1, .5f));
+//            addColoredVertex(builder, mx, fieldColor, new Vector3f(1, 1, .5f));
+//            addColoredVertex(builder, mx, fieldColor, new Vector3f(1, 0, .5f));
+//            addColoredVertex(builder, mx, fieldColor, new Vector3f(0, 0, .5f));
 
 //            lines.pos(x2, y2, z1).color(cR, cG, cB, cA).endVertex();
 //            lines.pos(x2, y1, z1).color(cR, cG, cB, cA).endVertex();
