347772f822e508b391418a53aa3fcc578ebb8e51
Ted Senft <code@robotgryphon.com>
2020-09-26 01:59:25 -0400

    Update logger access to public

--- src/main/java/com/robotgryphon/compactcrafting/CompactCrafting.java
+++ src/main/java/com/robotgryphon/compactcrafting/CompactCrafting.java
@@ -22,7 +22,7 @@ import org.apache.logging.log4j.Logger;
 public class CompactCrafting
 {
     // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
 
     public static final String MOD_ID = "compactcrafting";
 
