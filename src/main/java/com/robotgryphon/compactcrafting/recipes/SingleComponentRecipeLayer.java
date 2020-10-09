b5d588c227abca2046b9c99795ee07f83ff57cbf
Ted Senft <code@robotgryphon.com>
2020-10-09 04:33:24 -0400

    Remove commented code from SCRL

--- src/main/java/com/robotgryphon/compactcrafting/recipes/SingleComponentRecipeLayer.java
+++ src/main/java/com/robotgryphon/compactcrafting/recipes/SingleComponentRecipeLayer.java
@@ -38,50 +38,6 @@ public class SingleComponentRecipeLayer implements IRecipeLayer {
         return false;
     }
 
-//    @Override
-//    public boolean matchesFieldLayer(IWorldReader world, MiniaturizationRecipe recipe, FieldProjectionSize fieldSize, AxisAlignedBB fieldLayer) {
-//        Optional<BlockState> component = recipe.getRecipeComponent(componentKey);
-//        // We can't find a component definition in the recipe, so something very wrong happened
-//        if(!component.isPresent()) {
-//            CompactCrafting.LOGGER.warn(
-//                String.format("Attempted to find component with key '%s' but no component was found.", componentKey)
-//            );
-//
-//            return false;
-//        }
-//
-//        // Dimensions check - make sure the field layer is large enough for the recipe layer
-//        if(this.dimensions.getXSize() > fieldLayer.getXSize() || this.dimensions.getZSize() > fieldLayer.getZSize())
-//            return false;
-//
-//        // Non-empty field positions
-//        BlockPos[] fieldPositions = BlockPos.getAllInBox(fieldLayer)
-//                .filter(p -> !world.isAirBlock(p))
-//                .map(BlockPos::toImmutable)
-//                .toArray(BlockPos[]::new);
-//
-//        // Get all the distinct block types in the current layer
-//        Stream<BlockState> distinctStates = Arrays.stream(fieldPositions)
-//                .map(world::getBlockState)
-//                .distinct();
-//
-//        // Invalid block found - early exit
-//        if(distinctStates.anyMatch(state -> state != component.get()))
-//            return false;
-//
-//        BlockPos[] normalizedFilledPositions = RecipeHelper.normalizeLayerPositions(this.dimensions, fieldPositions);
-//
-//        // Create a minimum-filled bounds of blocks in the field
-//        AxisAlignedBB trimmedBounds = BlockSpaceUtil.getBoundsForBlocks(Arrays.asList(normalizedFilledPositions));
-//
-//        // Whitespace trim done - no padding needed, min and max bounds are already correct
-//
-//        // Check recipe template against padded world layout
-//        Map<BlockPos, String> templateMap = RecipeHelper.remapLayerToRecipe(world, recipe, fieldSize, fieldLayer);
-//
-//        return RecipeHelper.layerMatchesTemplate(world, recipe, this, fieldLayer, normalizedFilledPositions);
-//    }
-
     @Override
     public AxisAlignedBB getDimensions() {
         return dimensions;
