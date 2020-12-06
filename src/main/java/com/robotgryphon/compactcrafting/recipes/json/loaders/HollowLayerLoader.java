dc335648d7a0770f91461538eb755c7870def2cb
Ted Senft <code@robotgryphon.com>
2020-12-06 14:00:49 -0500

    Swap hollow layer definition to use wall instead of component

--- src/main/java/com/robotgryphon/compactcrafting/recipes/json/loaders/HollowLayerLoader.java
+++ src/main/java/com/robotgryphon/compactcrafting/recipes/json/loaders/HollowLayerLoader.java
@@ -1,17 +1,17 @@
 package com.robotgryphon.compactcrafting.recipes.json.loaders;
 
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
+import com.robotgryphon.compactcrafting.recipes.json.RecipeLoadingException;
 import com.robotgryphon.compactcrafting.recipes.layers.HollowComponentRecipeLayer;
 import com.robotgryphon.compactcrafting.recipes.layers.IRecipeLayer;
 
 public class HollowLayerLoader implements ILayerLoader {
     @Override
-    public IRecipeLayer createLayerFromDefinition(JsonObject layer) {
-        if(!layer.has("component"))
-            throw new JsonParseException("Hollow layer definition does not have an associated component key.");
+    public IRecipeLayer createLayerFromDefinition(JsonObject layer) throws RecipeLoadingException {
+        if(!layer.has("wall"))
+            throw new RecipeLoadingException("Hollow layer definition does not have an associated component key (wall).");
 
-        String component = layer.get("component").getAsString();
+        String component = layer.get("wall").getAsString();
 
         return new HollowComponentRecipeLayer(component);
     }
--- src/main/java/com/robotgryphon/compactcrafting/recipes/json/loaders/ILayerLoader.java
+++ src/main/java/com/robotgryphon/compactcrafting/recipes/json/loaders/ILayerLoader.java
@@ -1,9 +1,10 @@
 package com.robotgryphon.compactcrafting.recipes.json.loaders;
 
 import com.google.gson.JsonObject;
+import com.robotgryphon.compactcrafting.recipes.json.RecipeLoadingException;
 import com.robotgryphon.compactcrafting.recipes.layers.IRecipeLayer;
 
 public interface ILayerLoader {
 
-    IRecipeLayer createLayerFromDefinition(JsonObject layer);
+    IRecipeLayer createLayerFromDefinition(JsonObject layer) throws RecipeLoadingException;
 }
