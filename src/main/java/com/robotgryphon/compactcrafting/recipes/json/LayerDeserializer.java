ff394f2b7a161899a9ef19df4bf484c394fb108e
Ted Senft <code@robotgryphon.com>
2020-12-06 14:00:26 -0500

    Update layer deserializer to not use Optional

--- src/main/java/com/robotgryphon/compactcrafting/recipes/json/LayerDeserializer.java
+++ src/main/java/com/robotgryphon/compactcrafting/recipes/json/LayerDeserializer.java
@@ -8,7 +8,6 @@ import com.robotgryphon.compactcrafting.recipes.json.loaders.MixedLayerLoader;
 import com.robotgryphon.compactcrafting.recipes.layers.IRecipeLayer;
 
 import java.lang.reflect.Type;
-import java.util.Optional;
 
 public class LayerDeserializer implements JsonDeserializer<IRecipeLayer> {
 
@@ -23,32 +22,35 @@ public class LayerDeserializer implements JsonDeserializer<IRecipeLayer> {
         if(!root.has("type"))
             throw new JsonParseException("Layer definition missing 'type' property.");
 
-        Optional<ILayerLoader> loader = Optional.empty();
+        ILayerLoader loader;
         String type = root.get("type").getAsString();
         switch(type.toLowerCase()) {
             case "mixed":
                 // Mixed layer type
-                loader = Optional.of(MIXED);
+                loader = MIXED;
                 break;
 
             case "filled":
             case "solid":
                 // Filled layer type
-                loader = Optional.of(FILLED);
+                loader = FILLED;
                 break;
 
             case "hollow":
                 // Hollow layer type
-                loader = Optional.of(HOLLOW);
+                loader = HOLLOW;
                 break;
 
             default:
                 throw new JsonParseException("Unknown layer type '" + type + "'");
         }
 
-        return loader
-                .map(iLayerLoader -> iLayerLoader.createLayerFromDefinition(root))
-                .orElse(null);
+        try {
+            return loader.createLayerFromDefinition(root);
+        }
 
+        catch(RecipeLoadingException rle) {
+            return null;
+        }
     }
 }
