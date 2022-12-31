b9be5df2a4cfa5410fabaea1a0c539101af6f4ac
Ted Senft <code@robotgryphon.com>
2022-12-30 23:36:40 -0500

    Remove multi-match modes, to be implemented on another branch

--- forge-main/src/main/java/dev/compactmods/crafting/proxies/block/MatchFieldProxyBlock.java
+++ forge-main/src/main/java/dev/compactmods/crafting/proxies/block/MatchFieldProxyBlock.java
@@ -3,35 +3,20 @@ package dev.compactmods.crafting.proxies.block;
 import javax.annotation.Nullable;
 
 import dev.compactmods.crafting.core.CCCapabilities;
-import dev.compactmods.crafting.proxies.MatchProxyMode;
 import dev.compactmods.crafting.proxies.data.MatchFieldProxyEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
-
-import java.util.Random;
 
 public class MatchFieldProxyBlock extends FieldProxyBlock implements EntityBlock {
 
-    public static EnumProperty<MatchProxyMode> MATCH_MODE = EnumProperty.create("mode", MatchProxyMode.class);
-
     public MatchFieldProxyBlock(Properties props) {
         super(props);
-        registerDefaultState(getStateDefinition().any().setValue(MATCH_MODE, MatchProxyMode.CONSTANT));
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        super.createBlockStateDefinition(builder);
-        builder.add(MATCH_MODE);
     }
 
     @Override
@@ -67,15 +52,5 @@ public class MatchFieldProxyBlock extends FieldProxyBlock implements EntityBlock
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new MatchFieldProxyEntity(pos, state);
-    }
-
-    @Override
-    public void tick(BlockState pState, ServerLevel level, BlockPos pos, Random rand) {
-        // Similar to redstone repeaters, a scheduled tick turns off a pulsing field proxy
-        final var mode = pState.getValue(MATCH_MODE);
-        if(mode.isPulse) {
-            var update = pState.setValue(SIGNAL, 0);
-            level.setBlock(pos, update, Block.UPDATE_ALL);
-        }
     }
 }
