41822efbbeb2b02108347a0bd82a75552da0660f
Ted Senft <code@robotgryphon.com>
2022-12-30 23:35:57 -0500

    Make proxies unable to be placed if not linked

--- forge-main/src/main/java/dev/compactmods/crafting/proxies/item/FieldProxyItem.java
+++ forge-main/src/main/java/dev/compactmods/crafting/proxies/item/FieldProxyItem.java
@@ -2,6 +2,8 @@ package dev.compactmods.crafting.proxies.item;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Optional;
+
 import dev.compactmods.crafting.core.CCCapabilities;
 import dev.compactmods.crafting.projector.FieldProjectorBlock;
 import dev.compactmods.crafting.projector.FieldProjectorEntity;
@@ -9,6 +11,7 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
@@ -19,6 +22,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -29,24 +33,31 @@ public class FieldProxyItem extends BlockItem {
         super(block, props);
     }
 
+    public static boolean isLinked(ItemStack stack) {
+        if (!stack.hasTag()) return false;
+        CompoundTag tag = stack.getTag();
+        return tag != null && tag.contains("field", Tag.TAG_COMPOUND);
+    }
+
+    public static Optional<BlockPos> getFieldPosition(ItemStack stack) {
+        if (!isLinked(stack)) return Optional.empty();
+        final var fieldInfo = stack.getTagElement("field");
+        if (fieldInfo == null || !fieldInfo.contains("center")) return Optional.empty();
+        final var fieldCenter = NbtUtils.readBlockPos(fieldInfo.getCompound("center"));
+        return Optional.of(fieldCenter);
+    }
+
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> text, TooltipFlag flags) {
-
-        boolean isLinked = false;
-        if(stack.hasTag()) {
-            CompoundTag field = stack.getOrCreateTagElement("field");
-            if(field.contains("center")) {
-                isLinked = true;
-
-                BlockPos linkedCenter = NbtUtils.readBlockPos(field.getCompound("center"));
-                text.add(new TranslatableComponent("tooltip.compactcrafting.proxy_bound", linkedCenter)
+        boolean isLinked = isLinked(stack);
+        getFieldPosition(stack).ifPresent(linkedCenter -> {
+            text.add(new TranslatableComponent("tooltip.compactcrafting.proxy_bound", linkedCenter)
                     .withStyle(ChatFormatting.ITALIC).withStyle(ChatFormatting.AQUA));
-            }
-        }
+        });
 
-        if(!isLinked) {
+        if (!isLinked) {
             text.add(new TranslatableComponent("tooltip.compactcrafting.proxy_bind_hint")
-                .withStyle(ChatFormatting.DARK_GRAY));
+                    .withStyle(ChatFormatting.DARK_GRAY));
         } else {
             text.add(new TranslatableComponent("tooltip.compactcrafting.proxy_unbind_hint")
                     .withStyle(ChatFormatting.DARK_GRAY));
@@ -58,9 +69,15 @@ public class FieldProxyItem extends BlockItem {
     }
 
     @Override
+    protected boolean canPlace(BlockPlaceContext pContext, BlockState pState) {
+        final var stack = pContext.getItemInHand();
+        return isLinked(stack);
+    }
+
+    @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
-        if(player.isDiscrete() && hand == InteractionHand.MAIN_HAND) {
+        if (player.isDiscrete() && hand == InteractionHand.MAIN_HAND) {
             // used in the air while sneaking
             player.displayClientMessage(new TextComponent("clearing field data"), true);
 
