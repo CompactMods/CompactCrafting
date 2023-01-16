d5aceb8bf316b185cb9772a45675f48f406b6240
Ted Senft <code@robotgryphon.com>
2023-01-16 08:06:04 -0500

    Mostly fix #57 - numeric types are still precisely checked

--- forge-main/src/main/java/dev/compactmods/crafting/recipes/catalyst/ItemStackCatalystMatcher.java
+++ forge-main/src/main/java/dev/compactmods/crafting/recipes/catalyst/ItemStackCatalystMatcher.java
@@ -1,21 +1,22 @@
 package dev.compactmods.crafting.recipes.catalyst;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-import java.util.function.Predicate;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.compactmods.crafting.api.catalyst.CatalystType;
 import dev.compactmods.crafting.api.catalyst.ICatalystMatcher;
 import dev.compactmods.crafting.core.CCCatalystTypes;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.Tag;
+import net.minecraft.nbt.NbtUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.ForgeRegistryEntry;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Predicate;
 
 public class ItemStackCatalystMatcher extends ForgeRegistryEntry<CatalystType<?>>
         implements ICatalystMatcher, CatalystType<ItemStackCatalystMatcher> {
@@ -28,25 +29,29 @@ public class ItemStackCatalystMatcher extends ForgeRegistryEntry<CatalystType<?>
     private final Predicate<ItemStack> nbtMatcher;
 
     private Optional<CompoundTag> getNbtTag() {
-        return Optional.of(new CompoundTag());
+        return Optional.ofNullable(nbt);
     }
 
     private final Item item;
+    private final CompoundTag nbt;
 
     public ItemStackCatalystMatcher() {
         this.item = null;
         this.nbtMatcher = (stack) -> true;
+        this.nbt = null;
     }
 
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     public ItemStackCatalystMatcher(ResourceLocation item, Optional<CompoundTag> nbt) {
         this.item = ForgeRegistries.ITEMS.getValue(item);
         this.nbtMatcher = buildMatcher(nbt.orElse(null));
+        this.nbt = nbt.orElse(null);
     }
 
     public ItemStackCatalystMatcher(ItemStack stack) {
         this.item = stack.getItem();
-        this.nbtMatcher = (s) -> true;
+        this.nbtMatcher = buildMatcher(stack.getOrCreateTag());
+        this.nbt = stack.getTag();
     }
 
     private Predicate<ItemStack> buildMatcher(CompoundTag filter) {
@@ -56,33 +61,11 @@ public class ItemStackCatalystMatcher extends ForgeRegistryEntry<CatalystType<?>
         return (stack) -> {
             // filters defined but item has no nbt
             if(!stack.hasTag() && !filter.isEmpty()) return false;
-            return tagMatched(stack.getTag(), filter);
+            final var tag = stack.getTag();
+            Objects.requireNonNull(tag);
+            return NbtUtils.compareNbt(tag, filter, true);
         };
     }
-
-    private boolean tagMatched(CompoundTag node, CompoundTag filter) {
-        // filter: "mycompound: {}"
-        if(filter.isEmpty()) return true;
-
-        return filter.getAllKeys().stream().allMatch(key -> {
-            if (!node.contains(key))
-                return false;
-
-            final byte tagType = filter.getTagType(key);
-            if (tagType == Tag.TAG_COMPOUND) {
-                // nested properties, recurse deeper
-                return tagMatched(node.getCompound(key), filter.getCompound(key));
-            } else {
-                // all other key types are "primitives" - direct match time
-                Tag primitive = node.get(key);
-                if(primitive == null)
-                    return false;
-
-                return primitive.equals(filter.get(key));
-            }
-        });
-    }
-
 
     public ResourceLocation getItemId() {
         return item.getRegistryName();
@@ -99,9 +82,12 @@ public class ItemStackCatalystMatcher extends ForgeRegistryEntry<CatalystType<?>
 
     @Override
     public Set<ItemStack> getPossible() {
-        final HashSet<ItemStack> set = new HashSet<>();
-        set.add(new ItemStack(item));
-        return set;
+        if(nbt == null)
+            return Set.of(new ItemStack(item));
+
+        final var withNbt = new ItemStack(item, 1);
+        withNbt.setTag(nbt);
+        return Set.of(withNbt);
     }
 
     @Override
