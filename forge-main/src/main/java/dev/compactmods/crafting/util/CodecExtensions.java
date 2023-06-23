bf2732d4ba8dd24a91b9ea3d167384274e9ab608
Ted Senft <code@robotgryphon.com>
2023-06-23 01:05:00 -0400

    Document codecs a bit

--- forge-main/src/main/java/dev/compactmods/crafting/util/CodecExtensions.java
+++ forge-main/src/main/java/dev/compactmods/crafting/util/CodecExtensions.java
@@ -21,6 +21,10 @@ public class CodecExtensions {
                 bl -> DataResult.success(ForgeRegistries.BLOCKS.getKey(bl)))
              .stable();
 
+     /**
+      * Variant of the ItemStack codec that allows for some optional defaults such as a default
+      * stack size of 1.
+      */
      public static final Codec<ItemStack> FRIENDLY_ITEMSTACK = RecordCodecBuilder.create(i -> i.group(
              Registry.ITEM.byNameCodec().fieldOf("id").forGetter(ItemStack::getItem),
              Codec.INT.optionalFieldOf("Count", 1).forGetter(ItemStack::getCount),
