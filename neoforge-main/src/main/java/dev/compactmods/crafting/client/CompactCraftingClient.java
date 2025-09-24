715503701f0009da6bc6d2c533bdab19a4792b08
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-09-24 15:18:09 +0200

    fix server crash

--- neoforge-main/src/main/java/dev/compactmods/crafting/client/CompactCraftingClient.java
+++ neoforge-main/src/main/java/dev/compactmods/crafting/client/CompactCraftingClient.java
@@ -2,11 +2,12 @@ package dev.compactmods.crafting.client;
 
 import dev.compactmods.crafting.CompactCrafting;
 import dev.compactmods.crafting.client.render.field.MiniaturizationFieldRenderer;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 
-@Mod(CompactCrafting.MOD_ID)
+@Mod(value = CompactCrafting.MOD_ID, dist = Dist.CLIENT)
 public class CompactCraftingClient {
 
     public CompactCraftingClient(IEventBus modBus) {
